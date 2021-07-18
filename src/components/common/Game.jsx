7da3058fa60ff6dd83a48a679decfa4576fb4bc5
lulzsun <jminquach@gmail.com>
2021-07-17 18:54:55 -0700

    fullscreen experiment

--- src/components/common/Game.jsx
+++ src/components/common/Game.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
 
 export default function Game () {
+	const handle = useFullScreenHandle();
+
+	function handleOnClick(e) {
+		if(!handle.active) {
+			handle.enter();
+		}
+		else {
+			handle.exit();
+		}
+	}
+
 	return (
-		<div className="h-full bg-black flex items-center">
-			<p className="w-full text-center text-5xl">🔧🐒</p>
-		</div>
+		// i am a huge noob at flexbox and css in general, heres what i used to figure this out
+		// https://stackoverflow.com/questions/90178/make-a-div-fill-the-height-of-the-remaining-screen-space
+		<FullScreen className="h-full bg-black" handle={handle}>
+			<div className="h-full flex flex-col items-center">
+				<div className="flex-initial">
+					<b>top</b>
+				</div>
+				<div className="flex-auto">
+					<div className="h-full flex items-center" onClick={(e) => handleOnClick(e)}>
+						<p hidden={handle.active} className="text-center text-5xl select-none">🔧🐒</p>
+						<p hidden={!handle.active} className="text-center text-5xl select-none">🚀🌒</p>
+					</div>
+				</div>
+				<div className="flex-initial">
+					<b>bottom</b>
+				</div>
+			</div>
+		</FullScreen>
 	)
-}
+} 
