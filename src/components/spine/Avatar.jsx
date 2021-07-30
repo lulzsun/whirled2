c09ade98035742694123e7070dec9fb35745e9ed
lulzsun <jminquach@gmail.com>
2021-07-30 02:48:00 -0700

    cleanup avatar

--- src/components/spine/Avatar.jsx
+++ src/components/spine/Avatar.jsx
@@ -13,13 +13,12 @@ export default function Avatar (props) {
 
   const assetManager = new AssetManager((image) => {return new ThreeJsTexture(image)}, parentPath);
 	assetManager.loadText(skeletonFile);
-	const test = assetManager.loadTextureAtlas(atlasFile);
+	assetManager.loadTextureAtlas(atlasFile);
 
   const [loaded, setLoaded] = useState(assetManager.isLoadingComplete());
   const [spineMesh, setSpineMesh] = useState(null);
-  const mesh = useRef(), skelMesh = useRef();
+  const mesh = useRef();
   var lastFrameTime = Date.now() / 1000;
-  var skeletonMesh;
 
   useFrame(() => {
     if (assetManager.isLoadingComplete() && !loaded) {
@@ -35,45 +34,37 @@ export default function Avatar (props) {
 
       // Set the scale to apply during parsing, parse the file, and create a new skeleton.
       skeletonJson.scale = 0.4;
-      var skeletonData = skeletonJson.readSkeletonData(assetManager.assets[parentPath+skeletonFile]);
+      const skeletonData = skeletonJson.readSkeletonData(assetManager.assets[parentPath+skeletonFile]);
 
       // Create a SkeletonMesh from the data and attach it to the scene
-      skeletonMesh = new SkeletonMesh(skeletonData, function(parameters) {
-        parameters.depthTest = false;
+      const skeletonMesh = new SkeletonMesh(skeletonData, function(parameters) {
+        parameters.depthTest = true;
       });
       skeletonMesh.state.setAnimation(0, animation, true);
-      //mesh.current.add(skeletonMesh);
-      //console.log(skeletonMesh);
-      //console.log(mesh.current);
       setSpineMesh(skeletonMesh);
-      //skeletonMesh.update();
-
       setLoaded(true);
     }
     else {
       if(loaded) {
-        //mesh.current.rotation.x = mesh.current.rotation.y += 1 / 100
         var now = Date.now() / 1000;
         var delta = now - lastFrameTime;
         lastFrameTime = now;
 
-        if(skelMesh) {
+        //mesh.current.rotation.x = mesh.current.rotation.y += 1 / 100
+
+        if(spineMesh) {
           spineMesh.update(delta);
-          //skelMesh.current.update(delta);
         }
-           
-        //console.log(spineMesh);
       }
     }
   });
 
   if(spineMesh) {
-    console.log(spineMesh);
     return (
       <mesh {...props} ref={mesh} scale={1} >
-        <primitive ref={skelMesh} object={spineMesh} />
+        <primitive object={spineMesh} scale={0.01} />
         <boxGeometry args={[1, 1, 1]} />
-        <meshBasicMaterial attach="material" color="#ff0000" wireframe={true} />
+        <meshBasicMaterial attach="material" color="#ff0000" wireframe={false} />
       </mesh>
     )
   }
