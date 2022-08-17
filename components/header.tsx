f402f865900eee1e983a600d3c431444abfcb5e7
Jimmy Quach <jminquach@gmail.com>
2022-08-16 20:00:42 -0700

    feat: link tabs to pages
    including structure for pages that have not yet been implemented

--- components/header.tsx
+++ components/header.tsx
@@ -2,6 +2,7 @@ import { Avatar, Group, Menu, Tabs, UnstyledButton } from "@mantine/core";
 import { IconBasket, IconArmchair2, IconBrandAppleArcade, 
   IconDoor, IconUser, IconWorld, IconLogout } from "@tabler/icons";
 import { supabaseClient } from '@supabase/auth-helpers-nextjs';
+import Link from "next/link";
 
 export default function Header() {
   return (
@@ -13,12 +14,47 @@ export default function Header() {
         <div className="flex flex-row space-x-4 pr-2">
           <Tabs className="self-end">
             <Tabs.List position="right">
-              <Tabs.Tab value="me" icon={<IconUser size={15} />}>Me</Tabs.Tab>
-              <Tabs.Tab value="stuff" icon={<IconArmchair2 size={15} />}>Stuff</Tabs.Tab>
-              <Tabs.Tab value="games" icon={<IconBrandAppleArcade size={15} />}>Games</Tabs.Tab>
-              <Tabs.Tab value="rooms" icon={<IconDoor size={15} />}>Rooms</Tabs.Tab>
-              <Tabs.Tab value="group" icon={<IconWorld size={15} />}>Groups</Tabs.Tab>
-              <Tabs.Tab value="shop" icon={<IconBasket size={15} />}>Shop</Tabs.Tab>
+              <Tabs.Tab value="me" icon={<IconUser size={15}/>}>
+                <Link href={"/profile"}>
+                  <a className="absolute inset-0 w-full h-full flex items-center" style={{textIndent: '32px'}}>Me</a>
+                </Link>
+                <span className="text-transparent">Me</span>
+              </Tabs.Tab>
+
+              <Tabs.Tab value="stuff" icon={<IconArmchair2 size={15}/>}>
+                <Link href={"/stuff"}>
+                  <a className="absolute inset-0 w-full h-full flex items-center" style={{textIndent: '32px'}}>Stuff</a>
+                </Link>
+                <span className="text-transparent">Stuff</span>
+              </Tabs.Tab>
+
+              <Tabs.Tab value="games" icon={<IconBrandAppleArcade size={15}/>}>
+                <Link href={"/games"}>
+                  <a className="absolute inset-0 w-full h-full flex items-center" style={{textIndent: '32px'}}>Games</a>
+                </Link>
+                <span className="text-transparent">Games</span>
+              </Tabs.Tab>
+              
+              <Tabs.Tab value="rooms" icon={<IconDoor size={15}/>}>
+                <Link href={"/rooms"}>
+                  <a className="absolute inset-0 w-full h-full flex items-center" style={{textIndent: '32px'}}>Rooms</a>
+                </Link>
+                <span className="text-transparent">Rooms</span>
+              </Tabs.Tab>
+
+              <Tabs.Tab value="groups" icon={<IconWorld size={15}/>}>
+                <Link href={"/groups"}>
+                  <a className="absolute inset-0 w-full h-full flex items-center" style={{textIndent: '32px'}}>Groups</a>
+                </Link>
+                <span className="text-transparent">Groups</span>
+              </Tabs.Tab>
+
+              <Tabs.Tab value="shop" icon={<IconBasket size={15}/>}>
+                <Link href={"/shop"}>
+                  <a className="absolute inset-0 w-full h-full flex items-center" style={{textIndent: '32px'}}>Shop</a>
+                </Link>
+                <span className="text-transparent">Shop</span>
+              </Tabs.Tab>
             </Tabs.List>
           </Tabs>
           <Group className="p-1" position="center">
---
+++ pages/shop.tsx
@@ -0,0 +1,24 @@
+import { useUser } from "@supabase/auth-helpers-react";
+import router from "next/router";
+import { useContext, useEffect } from "react";
+import { PagePaneContext } from "./_app";
+
+export default function Shop() {
+  const { user, isLoading } = useUser();
+  const {isPageVisible, setIsPageVisible} = useContext(PagePaneContext);
+
+  useEffect(() => {
+    if(!isLoading) {
+      if (user) {
+        setIsPageVisible(true);
+      }
+      else {
+        router.push('/login');
+      }
+    }
+  }, [isLoading, user]);
+
+  return (
+    <div>hi from shop</div>
+  );
+}
