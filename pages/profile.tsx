8b03128629a46e8cbbc0079305c33d4b1c47a9ca
Jimmy Quach <jminquach@gmail.com>
2022-08-16 19:59:26 -0700

    feat: functional basic profile page

---
+++ pages/profile.tsx
@@ -0,0 +1,18 @@
+import { useUser } from "@supabase/auth-helpers-react";
+import router from "next/router";
+import { useContext, useEffect } from "react";
+
+export default function Profile() {
+  const { user, isLoading } = useUser();
+
+  useEffect(() => {
+    if(!isLoading) {
+      if (user) {
+        router.push(`/profile/${user?.user_metadata['username']}`);
+      }
+      else {
+        router.push('/login');
+      }
+    }
+  }, [isLoading, user]);
+}
---
+++ pages/profile/[username].tsx
@@ -0,0 +1,57 @@
+import { createClient } from "@supabase/supabase-js";
+import { GetServerSidePropsContext } from "next";
+import { useContext, useEffect } from "react";
+import { PagePaneContext } from "../_app";
+
+interface PageProps {
+  pageData: {
+    profile: Profile;
+    errors?: string;
+  };
+}
+
+interface Profile {
+  username: string;
+  avatar_url: string;
+  birthday?: string;
+}
+
+export default function Id({ pageData }: PageProps) {
+  const {isPageVisible, setIsPageVisible} = useContext(PagePaneContext);
+
+  useEffect(() => {
+    setIsPageVisible(true);
+  }, []);
+
+  const profile = pageData.profile;
+  
+  return (
+  <>
+    <div>user: {profile.username}</div>
+    <div>birthday: {profile.birthday}</div>
+    <div>avatar_url: {profile.avatar_url}</div>
+  </>
+  );
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  try {
+    // Create a client with service role key
+    const supabaseAdmin = createClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+      process.env.SUPABASE_SERVICE_ROLE_KEY || ''
+    );
+
+    const username = context.params?.username;
+    // Run queries with service role (bypasses RLS) on the server and
+    // find row matching our page's username
+    const { data: profile } = await supabaseAdmin.from('profiles').select('*').eq('username', username).single();
+    if(profile == null) return { props: { pageData: {error: "Profile does not exist" }} }
+
+    // By returning { props: pageData: {profile} }, the StaticPropsDetail component
+    // will receive `pageData` as a prop at build time
+    return { props: { pageData: {profile} } }
+  } catch (err: any) {
+    return { props: { pageData: {error: err.message }} }
+  }
+};
