e9006d5fe20db2ba8db42fc63d79654e6c3d00cd
Jimmy Quach <jminquach@gmail.com>
2022-08-21 01:16:08 -0700

    feat: sql function to get profile comments

--- sql/functions.sql
+++ sql/functions.sql
@@ -1,5 +1,6 @@
 -- Create a function & trigger to insert a profile after inserting into auth.users
-CREATE OR REPLACE function public.create_profile_for_user()
+DROP FUNCTION IF EXISTS public.create_profile_for_user;
+CREATE function public.create_profile_for_user()
 returns trigger as $$
 begin
   insert into public.profiles(id, username, nickname, birthday)
@@ -14,7 +15,8 @@ CREATE trigger on_auth_user_created
   for each row execute procedure public.create_profile_for_user();
 
 -- Create a function & trigger to insert stuff after inserting into profiles
-CREATE OR REPLACE function public.create_stuff_for_user()
+DROP FUNCTION IF EXISTS public.create_stuff_for_user;
+CREATE function public.create_stuff_for_user()
 returns trigger as $$
 begin
   insert into public.stuff(id)
@@ -27,3 +29,34 @@ DROP trigger IF EXISTS on_auth_user_created on public.profiles;
 CREATE trigger on_auth_user_created
   after insert on public.profiles
   for each row execute procedure public.create_stuff_for_user();
+
+-- Create a function to retrieve profile comments with limits
+DROP FUNCTION IF EXISTS public.get_profile_comments;
+CREATE FUNCTION public.get_profile_comments(_profile_id uuid, parent_offset bigint, parent_limit bigint, max_depth bigint)
+RETURNS TABLE (id bigint, parent_id bigint, content text, depth bigint)
+AS $$
+BEGIN
+  RETURN QUERY
+  with recursive entries (id, parent_id) as (
+    (
+      select 
+        pc.id, pc.parent_id, pc.content,
+        CAST(0 as bigint) as _depth
+      from comments as pc
+      where pc.parent_id is null and pc.profile_id = _profile_id
+      order by pc.id 
+      limit parent_limit -- max root comments
+      offset parent_offset -- offset root comments (for pagination)
+    ) 
+    union all
+    (
+      select 
+        comments.id, comments.parent_id, comments.content,
+        _depth+1 as _depth
+      from entries inner join comments on (comments.parent_id = entries.id) 
+      where not _depth = max_depth -- max child comments
+    )
+  ) 
+  table entries;
+END;
+$$ LANGUAGE plpgsql;
