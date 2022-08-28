ca4e43c9b59066786e0faf0f647739e5eb62d26e
Jimmy Quach <jminquach@gmail.com>
2022-08-28 12:43:34 -0700

    chore: copy profiles table schema from supabase

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,3 +1,17 @@
+create policy "Allow individual read access" on public.profiles for select using ( auth.uid() = id );
+create policy "Allow individual insert access" on public.profiles for insert with check ( auth.uid() = id );
+create policy "Allow individual update access" on public.profiles for update using ( auth.uid() = id );
+
+CREATE TABLE profiles (
+    id uuid unique primary key references auth.users (id),
+    created_at timestamp with time zone default now() not null,
+    username unique text not null,
+    nickname text not null,
+    birthday date not null,
+    avatar_url text not null,
+    is_deleted boolean default false not null
+);
+
 CREATE TABLE comments (
     id bigint unique primary key GENERATED ALWAYS AS IDENTITY,
     profile_id uuid not null references profiles (id),
@@ -5,7 +19,7 @@ CREATE TABLE comments (
     parent_id bigint references comments (id),
     created_at timestamp with time zone default now() not null,
     updated_at timestamp with time zone default now() null,
-    content text null,
+    content text not null,
     is_deleted boolean default false not null
 );
 
