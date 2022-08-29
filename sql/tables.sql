d13d9b58903643839eda59e56ba633cb55382fd0
Jimmy Quach <jminquach@gmail.com>
2022-08-28 22:04:53 -0700

    feat: add rls policies

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,31 +1,46 @@
+-- Create profiles table
+CREATE TABLE profiles (
+  id uuid unique primary key references auth.users (id),
+  created_at timestamp with time zone default now() not null,
+  username unique text not null CHECK (char_length(username) <= 15),
+  nickname text not null CHECK (char_length(nickname) <= 15),
+  birthday date not null,
+  avatar_url text not null,
+  is_deleted boolean default false not null
+);
+
 create policy "Allow individual read access" on public.profiles for select using ( auth.uid() = id );
 create policy "Allow individual insert access" on public.profiles for insert with check ( auth.uid() = id );
 create policy "Allow individual update access" on public.profiles for update using ( auth.uid() = id );
+alter table public.profiles enable row level security;
 
-CREATE TABLE profiles (
-    id uuid unique primary key references auth.users (id),
-    created_at timestamp with time zone default now() not null,
-    username unique text not null,
-    nickname text not null,
-    birthday date not null,
-    avatar_url text not null,
-    is_deleted boolean default false not null
-);
-
+-- Create comments table
 CREATE TABLE comments (
-    id bigint unique primary key GENERATED ALWAYS AS IDENTITY,
-    profile_id uuid not null references profiles (id),
-    user_id uuid not null references profiles (id),
-    parent_id bigint references comments (id),
-    created_at timestamp with time zone default now() not null,
-    updated_at timestamp with time zone default now() null,
-    content text not null,
-    is_deleted boolean default false not null
+  id bigint unique primary key GENERATED ALWAYS AS IDENTITY,
+  profile_id uuid not null references profiles (id),
+  user_id uuid not null references profiles (id) default uid(),
+  parent_id bigint references comments (id),
+  created_at timestamp with time zone default now() not null,
+  updated_at timestamp with time zone default now() null,
+  content text not null CHECK (char_length(content) <= 255),
+  is_deleted boolean default false not null
 );
 
+create policy "Allow individual read access" on public.comments for select using ( auth.uid() = user_id );
+create policy "Allow individual insert access" on public.comments for insert with check ( auth.uid() = user_id );
+create policy "Allow individual update access" on public.comments for update using ( auth.uid() = user_id );
+alter table public.comments enable row level security;
+
+-- Create votes table
 CREATE TABLE votes (
-    user_id uuid not null references profiles (id),
-    comment_id bigint not null references comments (id),
-    is_upvote boolean not null,
-    PRIMARY KEY(user_id, comment_id)
+  user_id uuid not null references profiles (id) default uid(),
+  comment_id bigint not null references comments (id),
+  value int default 0 not null,
+  PRIMARY KEY(user_id, comment_id),
+  constraint vote_quantity check (value <= 1 and value >= -1)
 );
+
+create policy "Allow individual read access" on public.votes for select using ( auth.uid() = user_id );
+create policy "Allow individual insert access" on public.votes for insert with check ( auth.uid() = user_id );
+create policy "Allow individual update access" on public.votes for update using ( auth.uid() = user_id );
+alter table public.votes enable row level security;
