8ac80056ae0c476bca2f4817dcf9a6b4b90a664a
Jimmy Quach <jminquach@gmail.com>
2022-09-02 16:22:44 -0700

    refactor: move allotment custom hook to main.tsx

--- components/main.tsx
+++ components/main.tsx
@@ -1,4 +1,6 @@
-import { Allotment } from "allotment";
+import { Allotment, AllotmentProps } from "allotment";
+import { PaneProps } from "allotment/dist/types/src/allotment";
+import { useRef, useState, ComponentType, useEffect } from "react";
 import { useRecoilState } from "recoil";
 import { pageVisibilty } from "../recoil/pageVisibility.recoil";
 import Game from "./game";
@@ -10,6 +12,33 @@ type Props = {
 
 export default function Main({children} : Props) {
   const [isPageVisible] = useRecoilState(pageVisibilty);
+
+  // https://github.com/johnwalley/allotment/issues/81
+  // all this below can be wrapped into useAllotment hook or smth like that
+  const isMountedRef = useRef(false);
+  const [Allotment, setAllotment] = useState<
+    (ComponentType<AllotmentProps> & { Pane: ComponentType<PaneProps> }) | null
+  >(null);
+
+  useEffect(() => {
+    isMountedRef.current = true;
+    import("allotment").then((mod) => {
+      if (!isMountedRef.current) {
+        return;
+      }
+      setAllotment(mod.Allotment);
+    }).catch((err) =>
+      console.error(err, `could not import allotment ${err.message}`)
+    );
+    return () => {
+      isMountedRef.current = false;
+    };
+  }, []);
+
+  if (!Allotment) {
+    return <div>loading...</div>;
+  }
+  // end of hook
   
   return (
     <div className='flex flex-col h-screen'>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,46 +2,15 @@ import '../styles/globals.css'
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { createEmotionCache, MantineProvider } from '@mantine/core';
-import { AllotmentProps } from "allotment";
 import "allotment/dist/style.css";
-import { ComponentType, createContext, Dispatch, SetStateAction, useEffect, useRef, useState } from 'react';
-import { PaneProps } from 'allotment/dist/types/src/allotment';
 import Main from '../components/main';
 import { UserProvider } from '@supabase/auth-helpers-react';
 import { supabaseClient } from '@supabase/auth-helpers-nextjs';
-import { RecoilRoot, useRecoilState } from 'recoil';
-import { pageVisibilty } from '../recoil/pageVisibility.recoil';
+import { RecoilRoot } from 'recoil';
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
   const emotionCache = createEmotionCache({ key: 'mantine', prepend: false }); // fixes tailwind
-
-  // https://github.com/johnwalley/allotment/issues/81
-  // all this below can be wrapped into useAllotment hook or smth like that
-  const isMountedRef = useRef(false);
-  const [Allotment, setAllotment] = useState<
-    (ComponentType<AllotmentProps> & { Pane: ComponentType<PaneProps> }) | null
-  >(null);
-
-  useEffect(() => {
-    isMountedRef.current = true;
-    import("allotment").then((mod) => {
-      if (!isMountedRef.current) {
-        return;
-      }
-      setAllotment(mod.Allotment);
-    }).catch((err) =>
-      console.error(err, `could not import allotment ${err.message}`)
-    );
-    return () => {
-      isMountedRef.current = false;
-    };
-  }, []);
-
-  if (!Allotment) {
-    return <div>loading...</div>;
-  }
-  // end of hook
 
   return (
     <>
