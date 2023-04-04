2d1440ae0b2c4b95be70a8b0b0b99e8879058cec
Jimmy Quach <jminquach@gmail.com>
2023-04-04 01:34:52 -0700

    -- START OVER, BLANK SLATE --
    invention requires two things:
    
    1. the ability to try a lot of experiments.
    2. not having to live with the collateral damage of failed experiments.

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-
-const withInterceptStdout = require('next-intercept-stdout');
-module.exports = withInterceptStdout({
-    reactStrictMode: true,
-    swcMinify: true,
-  },
-  (text) => (text.includes('Duplicate atom key') ? '' : text),
-);
--- recoil/pageVisibility.recoil.ts
+++
@@ -1,6 +0,0 @@
-import { atom } from 'recoil';
-
-export const pageVisibiltyState = atom({
-  key: 'pageVisibiltyState',
-  default: false,
-});
