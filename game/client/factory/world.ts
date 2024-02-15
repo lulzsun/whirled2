0a24037c9c910e88e39f50bcb9728c7ea13d9956
Jimmy Quach <jminquach@gmail.com>
2024-02-15 12:25:42 -0800

    feat: update world camera to use orthographic

--- game/client/factory/world.ts
+++ game/client/factory/world.ts
@@ -6,7 +6,7 @@ import { Entity } from "./entity";
 
 export type World = {
 	objects: Map<number, Entity>;
-	camera: THREE.PerspectiveCamera;
+	camera: THREE.Camera;
 	scene: THREE.Scene;
 	renderer: THREE.WebGLRenderer;
 	time: {
@@ -22,10 +22,16 @@ export const createWorld = (): World => {
 	const canvas = document.querySelector<HTMLDivElement>("#app")!;
 
 	const light = new THREE.DirectionalLight(0xffffff, 3);
-	world.camera = new THREE.PerspectiveCamera(
-		70,
-		window.innerWidth / window.innerHeight,
-		1,
+	const aspect = window.innerWidth / window.innerHeight;
+	const fov = 1000;
+
+	// world.camera = new THREE.PerspectiveCamera(70, aspect, 0.1, 1000);
+	world.camera = new THREE.OrthographicCamera(
+		(-fov * aspect) / 2,
+		(fov * aspect) / 2,
+		fov / 2,
+		-fov / 2,
+		0.1,
 		1000,
 	);
 	world.camera.position.z = 400;
@@ -37,8 +43,17 @@ export const createWorld = (): World => {
 	world.renderer.setSize(window.innerWidth, window.innerHeight);
 
 	window.addEventListener("resize", () => {
-		world.camera.aspect = window.innerWidth / window.innerHeight;
-		world.camera.updateProjectionMatrix();
+		const aspect = window.innerWidth / window.innerHeight;
+		if (world.camera instanceof THREE.PerspectiveCamera) {
+			world.camera.aspect = aspect;
+			world.camera.updateProjectionMatrix();
+		} else if (world.camera instanceof THREE.OrthographicCamera) {
+			world.camera.left = (-fov * aspect) / 2;
+			world.camera.right = (fov * aspect) / 2;
+			world.camera.top = fov / 2;
+			world.camera.bottom = -fov / 2;
+			world.camera.updateProjectionMatrix();
+		}
 
 		world.renderer.setSize(window.innerWidth, window.innerHeight);
 	});
