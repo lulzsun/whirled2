f7a65c3e051bb3eca2ab6a336ad4f79c4d5f2550
Jimmy Quach <jminquach@gmail.com>
2025-11-06 21:18:43 -0800

    fix: dev localhost issues with incorrect ipaddress
    some old issue

--- game/client/constants.ts
+++ game/client/constants.ts
@@ -1,5 +1,14 @@
 export const API_URL = (() => {
-	if (["127.0.0.1", "localhost"].includes(window.location.hostname))
-		return `${window.location.protocol}//${window.location.hostname}:42069`;
-	else return `${window.location.protocol}//${window.location.hostname}`;
+	const hostname = window.location.hostname;
+	const isLocalhost = ["127.0.0.1", "localhost"].includes(hostname);
+	const isIpAddress =
+		/^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(
+			hostname,
+		);
+
+	if (isLocalhost || isIpAddress) {
+		return `${window.location.protocol}//${hostname}:42069`;
+	} else {
+		return `${window.location.protocol}//${hostname}`;
+	}
 })();
